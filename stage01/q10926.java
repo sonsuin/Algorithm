cf7fedafbd160d759141362a707fd90cc024cda6
suin <suin2638@naver.com>
2022-05-08 02:56:51 +0900

    ??!

---
+++ stage01/q10926.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class q10926{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        //String 은 sc.Stringnext(); X
+        String nm = sc.next();
+        sc.close();;
+
+        System.out.print(nm +"??!");
+    }
+}

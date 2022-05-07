f4980c2f89ec9b42283fbbdf823e69f5953ae60e
suin <suin2638@naver.com>
2022-05-08 01:40:44 +0900

    새싹

---
+++ stage01/q25083.java
@@ -0,0 +1,11 @@
+public class q25083{
+//쌍따음표,\(백슬래시) 앞에 \(백슬래시) 넣어주기
+    public static void main(String[] args){
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+    }
+}

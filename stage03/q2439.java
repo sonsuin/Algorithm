a63854a287f94eada1998a45185d8da5937fa9ad
suin <suin2638@naver.com>
2022-05-11 23:34:54 +0900

    별찍기 BufferedWriter

--- stage03/q2439.java
+++ stage03/q2439.java
@@ -1,19 +1,27 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class q2439 {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        sc.close();
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        br.close();
+
         //첫번째 for문의 인덱스(i) -> 줄 의미
         for(int i=1; i<=N; i++){
             for(int j=1; j<=N-i; j++){
-                System.out.print(" ");
+                bw.write(" ");
             }
-            for(int j=1; j<=i; j++){
-                System.out.print("*");
+            for(int k=1; k<=i; k++){
+                bw.write("*");
              }
-        System.out.println();
+             bw.newLine();
        }
+       bw.flush();
+       bw.close();
     }
 }
